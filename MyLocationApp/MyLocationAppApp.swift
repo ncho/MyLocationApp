f845f12ec3875f03cf69a5bfc98e683d2c6a44c8
Nathan Cho <ncho@esri.com>
2025-12-23 11:58:26 -0800

    Initial Commit

---
+++ MyLocationApp/MyLocationAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyLocationAppApp.swift
+//  MyLocationApp
+//
+//  Created by Nathan Cho on 12/23/25.
+//
+
+import SwiftUI
+
+@main
+struct MyLocationAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MyLocationAppTests/MyLocationAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  MyLocationAppTests.swift
+//  MyLocationAppTests
+//
+//  Created by Nathan Cho on 12/23/25.
+//
+
+import Testing
+@testable import MyLocationApp
+
+struct MyLocationAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
